950f122100a81f1ddc3bc084de56e9d0d21d55fa
ShrikantThrymr <shri@bit007>
2017-07-28 18:00:00 +0530

    first commit

---
+++ app/src/main/java/com/example/thrymr/newexpensesapp/Utils/Constants.java
@@ -0,0 +1,12 @@
+package com.example.thrymr.newexpensesapp.Utils;
+
+
+public interface Constants {
+
+    String employee_name_fragment = "employee_name_fragment";
+    String TRIP="Trip";
+    String INDIVIDUAl="Individual";
+
+    String ADMIN_TRIP_FRAGMENT="ADMIN_TRIP_FRAGMENT";
+}
+
